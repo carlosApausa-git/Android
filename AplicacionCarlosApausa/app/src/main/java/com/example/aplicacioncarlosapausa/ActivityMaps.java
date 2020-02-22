98d1ace56f1b127c610429408b362d36ff28f5f0
carlosApausa-git <60218811+carlosApausa-git@users.noreply.github.com>
2020-02-22 19:24:42 +0100

    Create ActivityMaps.java

---
+++ AplicacionCarlosApausa/app/src/main/java/com/example/aplicacioncarlosapausa/ActivityMaps.java
@@ -0,0 +1,39 @@
+package com.example.aplicacioncarlosapausa;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ActivityMaps extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng restaurante = new LatLng(40.4602736, -3.7110846);
+        mMap.addMarker(new MarkerOptions().position(restaurante).title("Restaurante carlos apausa"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(restaurante, 15));
+    }
+
+}
